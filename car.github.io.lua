0b8eece226eb9c46014a55377d569e056d40831a
robIoxScript <cypruxy@gmail.com>
2025-01-28 18:31:51 +0100

    Update car.github.io.lua

--- car.github.io.lua
+++ car.github.io.lua
@@ -1,14 +1,9 @@
-
--- check game
-if PlaceId == 111130915266245 then
-   local CurrentVersion = "Symbiote Hub - Race Training v1"
-
    -- call the library
    local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
    -- main frame
    local Window = Rayfield:CreateWindow({
-      Name = CurrentVersion,
+      Name = "gay",
       Icon = 0, -- inutile
       LoadingTitle = "Symbiote Hub",
       LoadingSubtitle = "by cypruxy",
@@ -40,34 +35,6 @@ if PlaceId == 111130915266245 then
          Key = {"GoatCypruxy"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
       }
    })
-
-   -- Local variable
-
-
-
-   -- FarmTab creation
-   local FarmTab = Window:CreateTab("Auto Farm", 4483362458) -- Title, Image
-
-   local Toggle = FarmTab:CreateToggle({
-      Name = "",
-      CurrentValue = false,
-      Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-      Callback = function(Value)
-      while CurrentValue do
-      game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Join Race"):FireServer()
-      wait()
-      local args = {
-         [1] = "Auto Race",
-         [2] = true
-     }
-     
-     game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Set Stat"):FireServer(unpack(args))
-     wait()
-      end
-   end,
-   })
-
-
 
    Rayfield:LoadConfiguration()
 end
